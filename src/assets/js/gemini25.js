1f56b00e914f8cfbd626c5570b039222559a5076
Ivan Bogomolov <ivan@bogomolov.su>
2025-03-25 20:47:48 +0100

    gemini 2.5

---
+++ src/assets/js/gemini25.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const targetHeading = document.getElementById("example");
+
+  if (targetHeading) {
+    const iframe = document.createElement("iframe");
+    iframe.src = "/blog/html/gemini25.html";
+    iframe.style.width = "100%";
+    iframe.style.border = "none";
+    iframe.style.height = "480px";
+
+    targetHeading.parentNode.insertBefore(iframe, targetHeading.nextSibling);
+  }
+});
---
+++ src/content/posts/dependency-injection/index.md
@@ -0,0 +1,9 @@
+---
+date: 2025-03-25T07:49:30Z
+draft: true
+title: Dependency injection
+image: ""
+keywords: []
+---
+
+Ideological heir of [DI Container vs. Service Template (generator)]({{< relref "posts/golang-di-container-vs-service-template" >}})
